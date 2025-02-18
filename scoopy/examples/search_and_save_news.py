6a454234796c7bcce2cfd244d244cd50e501945c
Thor Whalen <1906276+thorwhalen@users.noreply.github.com>
2025-02-18 17:19:37 -0500

    add search_and_save_news function and current_time_str utility; update notebook with examples

---
+++ scoopy/examples/search_and_save_news.py
@@ -0,0 +1,34 @@
+import os
+from dol import JsonFiles
+from scoopy import search_news
+
+
+def current_time_str():
+    from datetime import datetime
+
+    now = datetime.now()
+    return now.strftime("%Y-%m-%d--%H-%M-%S")
+
+
+def search_and_save_news(query, store='~', country='us'):
+    from scoopy import search_news
+
+    if isinstance(store, str):
+        import os
+        from dol import JsonFiles
+
+        rootdir = os.path.expanduser(store)
+        # create the rootdir if it does not exist
+        os.makedirs(rootdir, exist_ok=True)
+
+        store = JsonFiles(rootdir)
+    results = search_news(query)
+    key = f'{current_time_str()}__{query}.json'
+    store[key] = results
+    return key
+
+
+if __name__ == '__main__':
+    import argh
+
+    argh.dispatch_command(search_and_save_news)
