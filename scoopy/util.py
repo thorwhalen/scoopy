c5f3dd1dd96612decfac89b693cacd9a1eb19759
Thor Whalen <1906276+thorwhalen@users.noreply.github.com>
2025-10-11 09:54:19 -0700

    refactor: Rename get_app_data_folder to get_app_config_folder

--- scoopy/util.py
+++ scoopy/util.py
@@ -3,13 +3,13 @@
 import os
 from functools import partial
 
-from i2 import get_app_data_folder
+from i2 import get_app_config_folder
 
 import dol
 
 pkg_name = "scoopy"
 
-_root_app_data_dir = get_app_data_folder()
+_root_app_data_dir = get_app_config_folder()
 app_data_dir = os.environ.get(
     f"{pkg_name.upper()}_APP_DATA_DIR",
     os.path.join(_root_app_data_dir, pkg_name),
